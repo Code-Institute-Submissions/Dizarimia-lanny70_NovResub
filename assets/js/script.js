d79cd652a6986a6990cd4b4a5b29f7babee0ac52
Dizarimia <lillemor.sunnegard@outlook.com>
2022-08-14 15:55:30 +0000

    get counter to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,73 @@
 const questions = [{
-    question: "How many times has he been married?",
-    options: {
-        option1: "1",
-        option2: "2",
-        option3: "3",
+        question: "How many times has he been married?",
+        options: {
+            option1: "1",
+            option2: "2",
+            option3: "3",
 
+        },
+        answer: "2"
     },
-    answer: "2"
-},
-{
-    question: "When is Lannys birthday?",
-    options: {
-        option1: "July 11th",
-        option2: "July 12th",
-        option3: "July 13th",
+    {
+        question: "When is Lannys birthday?",
+        options: {
+            option1: "July 11th",
+            option2: "July 12th",
+            option3: "July 13th",
 
+        },
+        answer: "July 13th"
     },
-    answer: "July 13th"
-},
-{
-    question: "What is Lannys favourite drink?",
-    options: {
-        option1: "Rum",
-        option2: "Whiskey",
-        option3: "Beer",
+    {
+        question: "What is Lannys favourite drink?",
+        options: {
+            option1: "Rum",
+            option2: "Whiskey",
+            option3: "Beer",
 
+        },
+        answer: "Beer"
     },
-    answer: "Beer"
-},
-{
-    question: "What is the name of the house Lanny designed?",
-    options: {
-        option1: "Solkullen",
-        option2: "Solbacken",
-        option3: "Solgården",
+    {
+        question: "What is the name of the house Lanny designed?",
+        options: {
+            option1: "Solkullen",
+            option2: "Solbacken",
+            option3: "Solgården",
 
+        },
+        answer: "Solbacken"
     },
-    answer: "Solbacken"
-},
-{
-    question: "What is Lannys favourite restaurant?",
-    options: {
-        option1: "Stekhuset Löberöd",
-        option2: "Vollsjö Pizzeria",
-        option3: "Noy's Thaikök Önneköp",
+    {
+        question: "What is Lannys favourite restaurant?",
+        options: {
+            option1: "Stekhuset Löberöd",
+            option2: "Vollsjö Pizzeria",
+            option3: "Noy's Thaikök Önneköp",
 
+        },
+        answer: "Stekhuset Löberöd"
     },
-    answer: "Stekhuset Löberöd"
-},
-{
-    question: "What is Lannys shoe size?",
-    options: {
-        option1: "41",
-        option2: "42",
-        option3: "43",
+    {
+        question: "What is Lannys shoe size?",
+        options: {
+            option1: "41",
+            option2: "42",
+            option3: "43",
 
+        },
+        answer: "42"
     },
-    answer: "42"
-},
-{
-    question: "What was Lannys name supposed to be?",
-    options: {
-        option1: "Larry",
-        option2: "Lenny",
-        option3: "Harry",
+    {
+        question: "What was Lannys name supposed to be?",
+        options: {
+            option1: "Larry",
+            option2: "Lenny",
+            option3: "Harry",
 
+        },
+        answer: "Lenny"
     },
-    answer: "Lenny"
-},
 ]
 
 const questionNumber = document.getElementById('question-number');
@@ -82,6 +82,7 @@ let totalScoreAchieved = 0;
 let activeQuestionNumber = 0;
 let TotalQuestions = 7;
 
+
 function main() {
 /* Load the quiz contents for the first question here */
 questionNumber.innerText = (activeQuestionNumber + 1);
@@ -89,8 +90,12 @@ question.innerText = questions[activeQuestionNumber].question;
 option1.innerText = questions[activeQuestionNumber].options.option1;
 option2.innerText = questions[activeQuestionNumber].options.option2;
 option3.innerText = questions[activeQuestionNumber].options.option3;
-document.getElementById("correctAnswerCount").innerHTML = correctAnswerCount;
-document.getElementById("totalScoreAchieved").innerHTML = totalScoreAchieved;
+renderCount()
+}
+
+function renderCount() {
+    document.getElementById("correctAnswerCount").innerHTML = correctAnswerCount;
+    document.getElementById("totalScoreAchieved").innerHTML = totalScoreAchieved;
 }
 
 function answer(optionId) {
@@ -113,7 +118,8 @@ activeQuestionNumber++;
 
 // check if game is over
 if (activeQuestionNumber === TotalQuestions) {
-    alert('Congratulations on finishing the questions');
+    renderCount()
+    alert(`Congratulations on finishing the questions! \nYour correct answers: ${correctAnswerCount} \nYour incorrect answers: ${totalScoreAchieved}`);
     return;
 }
 
